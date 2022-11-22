7cf27a77a9ce864876b47a87269304ac3de995e1
Alex Schmerber <alex.g.schmerber@gmail.com>
2022-11-22 11:33:32 -0600

    Javascript with HTML Exercises

---
+++ js/external.js
@@ -0,0 +1,11 @@
+"use strict"
+console.log("Hello from external Javascript")
+alert('Welcome to my Website!')
+let color = prompt('What is your favorite color??')
+alert(`Wow! ${color} is my favorite too! `)
+
+let brotherBear = prompt('How many days would you like to rent brother bear?');
+let theLittleMermaid = prompt('How many days would you like to rent Little Mermaid?');
+let Hercules = prompt('How many days would you like to rent Hercules?');
+
+alert('That will cost you ' + (parseInt(brotherBear) + parseInt(theLittleMermaid) + parseInt(Hercules)) * 3);
--- js/index.js
+++
@@ -1,4 +0,0 @@
-"use strict"
-let age = prompt('What is your age?')
-
-alert(`DocRob, you are ${age} old = WOW!!!`)
